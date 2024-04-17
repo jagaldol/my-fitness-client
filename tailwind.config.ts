2d451d4882530e16cc5f43688d64972c5e19adaf
jagaldol <yoy06056@pusan.ac.kr>
2024-04-17 10:52:08 +0900

    chore: yarn으로 변경 및 리팩토링

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: "#131623",
+        "main-theme": "#4064F6",
+        "content-box": "#1E2637",
+        "input-box": "#1C1E2C",
+        text: "#ffffff",
       },
+    },
+    fontFamily: {
+      GmarketSansMedium: ["GmarketSansMedium", "Arial", "Helvetica", "sans-serif"],
+      SUITRegular: ["SUIT-Regular", "Arial", "Helvetica", "sans-serif"],
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
