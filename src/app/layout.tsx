f3bf662c69670ac1d7f8b0ea12ff4953f2bc240c
jagaldol <yoy06056@pusan.ac.kr>
2024-05-01 14:06:23 +0900

    refactor: Change to file based icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,18 +8,6 @@ import ModalList from "@/components/modal/ModalList"
 export const metadata = {
   title: "Fitness | Behind",
   description: "당신만을 위한 비하인드 운동 관리 프로젝트",
-  manifest: "/favicons/site.webmanifest",
-  icons: {
-    icon: [{ url: "/favicons/favicon-32x32.png" }, { url: "/favicons/favicon-16x16.png" }],
-    shortcut: ["/shortcut-icon.png"],
-    apple: [{ url: "/favicons/apple-touch-icon.png" }],
-    other: [
-      {
-        rel: "mask-icon",
-        url: "/favicons/safari-pinned-tab.svg",
-      },
-    ],
-  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
