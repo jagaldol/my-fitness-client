0f57a4d50f70863de8abde18da1daa142b7cec75
jagaldol <yoy06056@pusan.ac.kr>
2024-05-03 18:03:18 +0900

    feat: 운동 존재 날짜 marker 추가

--- src/containers/home/MainCalendar.tsx
+++ src/containers/home/MainCalendar.tsx
@@ -1,13 +1,27 @@
 "use client"
 
-import React from "react"
+import React, { useState } from "react"
 import moment from "moment/moment"
 import Calendar from "react-calendar"
 import { useRecoilState } from "recoil"
 import selectedDateState from "@/states/selectedDateState"
+import { useQuery } from "@tanstack/react-query"
+import axiosInstance from "@/utils/axiosInstance"
 
 export default function MainCalendar() {
-  const [date, setDate] = useRecoilState(selectedDateState)
+  const [selectedDate, setSelectedDate] = useRecoilState(selectedDateState)
+
+  const [params, setParams] = useState({ month: moment(selectedDate).format("YYYY-MM") })
+
+  const { data, isFetched } = useQuery({
+    queryKey: ["/sessions/dates", params],
+    queryFn: async () => {
+      return axiosInstance.get("/sessions/dates", { params }).then((res) => {
+        return res.data.response.dates
+      })
+    },
+    staleTime: 300_000,
+  })
   return (
     <Calendar
       locale="ko"
@@ -15,12 +29,22 @@ export default function MainCalendar() {
       next2Label={null}
       prev2Label={null}
       minDetail="year"
-      formatDay={(locale, d) => moment(d).format("D")}
-      formatMonthYear={(locale, d) => moment(d).format("YYYY. MM")}
-      formatYear={(locale, d) => moment(d).format("YYYY")}
-      value={date}
+      formatDay={(locale, date) => moment(date).format("D")}
+      formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")}
+      formatYear={(locale, date) => moment(date).format("YYYY")}
+      tileClassName={({ date, view }) => {
+        if (isFetched && view === "month" && data.includes(moment(date).format("YYYY-MM-DD")))
+          return "react-calendar__tile-marker"
+        return ""
+      }}
+      value={selectedDate}
+      onActiveStartDateChange={({ activeStartDate, view }) => {
+        if (view === "month") {
+          setParams({ month: moment(activeStartDate).format("YYYY-MM") })
+        }
+      }}
       onChange={(selectedData) => {
-        setDate(selectedData as Date)
+        setSelectedDate(selectedData as Date)
       }}
     />
   )
