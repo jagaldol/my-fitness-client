3fc25c113eaf90820436a9226f92f8a064fc7bf2
jagaldol <yoy06056@pusan.ac.kr>
2024-05-03 20:53:10 +0900

    feat: 모바일 달력 swipe로 넘기기 설정

--- src/containers/home/MainCalendar.tsx
+++ src/containers/home/MainCalendar.tsx
@@ -7,9 +7,14 @@ import { useRecoilState } from "recoil"
 import selectedDateState from "@/states/selectedDateState"
 import { useQuery } from "@tanstack/react-query"
 import axiosInstance from "@/utils/axiosInstance"
+import { useSwipeable } from "react-swipeable"
+
+type View = "century" | "decade" | "year" | "month"
 
 export default function MainCalendar() {
   const [selectedDate, setSelectedDate] = useRecoilState(selectedDateState)
+  const [activeStartDateState, setActiveStartDateState] = useState(selectedDate)
+  const [viewState, setViewState] = useState<View>("month")
 
   const [params, setParams] = useState({ month: moment(selectedDate).format("YYYY-MM") })
 
@@ -22,9 +27,41 @@ export default function MainCalendar() {
     },
     staleTime: 300_000,
   })
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () =>
+      setActiveStartDateState((prevState) => {
+        const next = new Date(prevState)
+        switch (viewState) {
+          case "year":
+            next.setFullYear(prevState.getFullYear() + 1)
+            break
+          default:
+            next.setMonth(prevState.getMonth() + 1)
+        }
+        return next
+      }),
+    onSwipedRight: () =>
+      setActiveStartDateState((prevState) => {
+        const next = new Date(prevState)
+        switch (viewState) {
+          case "year":
+            next.setFullYear(prevState.getFullYear() - 1)
+            break
+          default:
+            next.setMonth(prevState.getMonth() - 1)
+        }
+        return next
+      }),
+  })
+
   return (
     <Calendar
+      inputRef={handlers.ref}
       locale="ko"
+      activeStartDate={activeStartDateState}
+      view={viewState}
+      onViewChange={({ view }) => setViewState(view)}
       calendarType="gregory"
       next2Label={null}
       prev2Label={null}
@@ -33,12 +70,13 @@ export default function MainCalendar() {
       formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")}
       formatYear={(locale, date) => moment(date).format("YYYY")}
       tileClassName={({ date, view }) => {
-        if (isFetched && view === "month" && data.includes(moment(date).format("YYYY-MM-DD")))
+        if (isFetched && view === "month" && data && data.includes(moment(date).format("YYYY-MM-DD")))
           return "react-calendar__tile-marker"
         return ""
       }}
       value={selectedDate}
       onActiveStartDateChange={({ activeStartDate, view }) => {
+        if (activeStartDate) setActiveStartDateState(activeStartDate)
         if (view === "month") {
           setParams({ month: moment(activeStartDate).format("YYYY-MM") })
         }
