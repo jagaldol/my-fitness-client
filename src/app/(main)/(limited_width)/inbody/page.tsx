a893bd3cb4f9e7dd4265ea22141e3f2c0ffba459
jagaldol <yoy06056@pusan.ac.kr>
2024-06-17 19:53:23 +0900

    feat: 기록 추가 상단으로 이동으로 인한 padding 축소

--- src/app/(main)/(limited_width)/inbody/page.tsx
+++ src/app/(main)/(limited_width)/inbody/page.tsx
@@ -2,7 +2,7 @@ import Inbody from "@/containers/more/inbody/Inbody"
 
 export default function InbodyPage() {
   return (
-    <div className="md:w-[700px] w-full align-middle px-3 pt-14 ">
+    <div className="md:w-[700px] w-full align-middle px-3 pt-5 ">
       <Inbody />
     </div>
   )
